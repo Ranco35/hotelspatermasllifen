0795dc968397f1c7fb5d963fd1e9eb7a7154c9cb
diego gajardo <diegobgajardo@gmail.com>
2021-07-09 11:28:47 -0400

    v6

---
+++ tutorial_add_javascript/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': "Tutorial adding assets",
+
+    'summary': """
+        Tutorial about how to create and use Javascript files in Odoo""",
+
+    'description': """
+        This module is a tutorial in the form of an app. In this app you can find the code to create and use
+        JavaScript functionalities in Odoo 14.
+    """,
+
+    'author': "Oocademy",
+    'website': "http://www.oocademy.com",
+    'price': 0.00,
+    'currency': 'EUR',
+    'category': 'Tutorial',
+    'version': '14.0.0.1',
+    'license': 'Other proprietary',
+    'depends': ['base','web'],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+
+    'data': [
+        'views/assets.xml',
+    ],
+    'application': True,
+}
